733ec05d48f3e3a7b6c838b375e20cd03f159198
Martin Gencur <mgencur@redhat.com>
2018-11-06 12:37:24 +0100

    Fix removal of -v flag in parameters
    * remove when https://github.com/knative/test-infra/pull/243 is
    integrated and knative/serving and openshift/knative-serving rebased to
    it

--- vendor/github.com/knative/test-infra/scripts/library.sh
+++ vendor/github.com/knative/test-infra/scripts/library.sh
@@ -220,7 +220,7 @@ function acquire_cluster_admin_role() {
 function report_go_test() {
   # Run tests in verbose mode to capture details.
   # go doesn't like repeating -v, so remove if passed.
-  local args=("${@/-v}")
+  local args=("${@/#-v}")
   local go_test="go test -race -v ${args[@]}"
   # Just run regular go tests if not on Prow.
   if (( ! IS_PROW )); then
